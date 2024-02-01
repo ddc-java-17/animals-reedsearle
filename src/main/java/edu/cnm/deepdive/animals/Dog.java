9c36869cdee7950d3e0a94b53a2f55a7a9c0b259
Reed Searle <csearle@cnm.edu>
2024-02-01 10:27:31 -0700

    Initial commit examples of polymorphism, inheritance

---
+++ src/main/java/edu/cnm/deepdive/animals/Dog.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive.animals;
+
+public class Dog extends Wolf {
+
+  protected Dog(String name) {
+    super(name);
+  }
+
+  @Override
+  public void vocalize() {
+    describe();
+    System.out.println("Bark!");  }
+
+  public void doTrick(){
+    System.out.println("Look at my trick! I'm the best dog!");
+  }
+
+  public static void describe() {
+    System.out.println("Domestic Dog (Canis Lupus Familiaris) is a divergent subspecies of Canis Lupus");
+  }
+
+}
---
+++ src/main/java/edu/cnm/deepdive/animals/Wolf.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive.animals;
+
+public class Wolf extends Canis {
+
+
+  protected Wolf(String name) {
+    super(name);
+  }
+
+  @Override
+  public void hunt() {
+    System.out.println("Hunt for live prey in packs.");
+  }
+
+  @Override
+  public void vocalize() {
+    describe();
+    System.out.println("Howl!");
+  }
+  public static void describe() {
+    System.out.println("Gray Wolf (Canus Lupus) is a species of the Canis genus");
+  }
+
+}
